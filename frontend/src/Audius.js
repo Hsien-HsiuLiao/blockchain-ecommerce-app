c5bc52db4372ebcf468b9cbb40ae93ccb9b44ab7
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-10-17 11:19:01 -0700

    update Audius component

--- frontend/src/Audius.js
+++ frontend/src/Audius.js
@@ -1,25 +1,84 @@
-import React from 'react';
-import { ethers } from 'ethers';
-import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+//import { ethers } from 'ethers';
+//import axios from 'axios';
+import { Button } from '@audius/stems';
 
-const API_URL = 'http://localhost:4000';
+const selectHost = async () => {
+    const sample = (arr) => arr[Math.floor(Math.random() * arr.length)]
+    const res = await fetch('https://api.audius.co')
+    const hosts = await res.json()
+    return sample(hosts.data)
+  }
 
-const ITEMS = [
-    {
-        id: 1,
-        price: ethers.utils.parseEther('100')
-    },
-    {
-        id: 2, 
-        price: ethers.utils.parseEther('200')
-    },
-];
 
 function Audius() { 
-    console.log('hello');
+    
+    const [track, setTrack] = useState(null)
+    const [host, setHost] = useState(null)
+  
+    useEffect(() => {
+      const fetchTrack = async () => {
+        const selectedHost = await selectHost()
+        const res = await fetch(`${selectedHost}/v1/tracks/trending?limit=1&timeRange=week`)
+        const json = await res.json()
+        const topTrack = json.data[0]
+        console.log("track2: ",json.data[1]);
+        setHost(selectedHost)
+        setTrack(topTrack)
+      }
+      fetchTrack()
+    }, [])
+  
+    const [audio, setAudio] = useState(null);
+    useEffect(() => {
+      if (track) {
+        console.log(track)
+        const id = track.id;
+        const streamUrl = `${host}/v1/tracks/${id}/stream`
+        const audio = new Audio(streamUrl)
+        setAudio(audio)
+  //      audio.play()
+      }
+    }, [track])
+  
+    const onPlay = useCallback(()=> {
+      if(audio) {
+        audio.play()
+      }
+    }, [audio])
 
-    return (
-        <h3>Audius component</h3>
+    const onPause = useCallback(()=> {
+        if(audio) {
+          audio.pause()
+        }
+      }, [audio])
+
+    return track && (
+        
+        <div className="topTrack">
+            
+      <div className="artwork">
+        <img src={track.artwork['150x150']} alt='artwork' />
+      </div>
+      <div className="title">
+        { track.title }
+      </div>
+      <div className="artist">
+        { track.user.name }
+      </div>
+      <div className="location">
+        { track.user.location }
+      </div>
+      <Button 
+        text='Play Track'
+        onClick={onPlay} />
+            
+            <button 
+        text='Pause Track'
+        onClick={onPause}>
+            Pause
+            </button>
+    </div>
     
     )
 }
