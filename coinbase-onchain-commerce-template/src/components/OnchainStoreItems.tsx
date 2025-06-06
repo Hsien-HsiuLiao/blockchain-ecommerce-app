7d3df092b40657648f8b07e98946a571f6a498b2
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-06-06 07:23:32 -0700

    add Audius component to store items

--- coinbase-onchain-commerce-template/src/components/OnchainStoreItems.tsx
+++ coinbase-onchain-commerce-template/src/components/OnchainStoreItems.tsx
@@ -1,15 +1,19 @@
 import OnchainStoreItem from './OnchainStoreItem';
 import { useOnchainStoreContext } from './OnchainStoreProvider';
+import Audius from './Audius/Audius';
 
 export default function OnchainStoreItems() {
   const { products } = useOnchainStoreContext();
 
   return (
-    <div className=" scroll mb-16 grow md:mb-0 ">
-      <div className="grid h-full grid-cols-1 grid-rows-2 sm:grid-cols-2">
-        {products?.map((item) => (
-          <OnchainStoreItem {...item} key={item.id} />
-        ))}
+    <div>
+      <Audius />
+      <div className=" scroll mb-16 grow md:mb-0 ">
+        <div className="grid h-full grid-cols-1 grid-rows-2 sm:grid-cols-2">
+          {products?.map((item) => (
+            <OnchainStoreItem {...item} key={item.id} />
+          ))}
+        </div>
       </div>
     </div>
   );
