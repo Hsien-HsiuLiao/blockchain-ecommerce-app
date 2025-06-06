4808ed3ae1cb7e750386c75561c1a3aa565088d1
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-06-06 07:27:09 -0700

    adjust Image className

--- coinbase-onchain-commerce-template/src/components/OnchainStoreItem.tsx
+++ coinbase-onchain-commerce-template/src/components/OnchainStoreItem.tsx
@@ -12,7 +12,9 @@ export default function OnchainStoreItem({ id, name, price, image }: Product) {
         <Image
           src={image}
           alt="123"
-          className="mx-auto object-contain max-sm:max-w-[300px] md:absolute md:h-full md:w-auto"
+          // className="mx-auto object-contain max-sm:max-w-[300px] md:absolute md:h-full md:w-auto"
+          className="mx-auto object-contain w-full h-auto max-w-[300px] sm:max-w-[400px] md:max-w-[500px]"
+
         />
       </div>
       <div className="flex items-center justify-between">
